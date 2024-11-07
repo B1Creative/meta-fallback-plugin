6a7556b8773eb7a190c3ba82cd53272be6f200ba
Connor <connor@connorgaunt.com>
2024-11-07 16:52:02 +0000

    Rewiring to make sure we're only using post data when singular

--- index.php
+++ index.php
@@ -13,13 +13,13 @@ if( ! defined( 'ABSPATH' ) ) {
 }
 
 // functions Function
-function kill(mixed $var) : never {
+function b1_mf__kill(mixed $var) : never {
     echo '<pre>';
     var_dump($var);
     echo '</pre>';
     exit;
 }
-function truncate(string $text, int $length = 160, string $appendix = '...'): string {
+function b1_mf__truncate(string $text, int $length = 160, string $appendix = '...'): string {
     $str = strip_tags( $text );
     $str = substr( $str, 0, $length );
     $str = rtrim( $str, ".,!?" );
@@ -27,11 +27,47 @@ function truncate(string $text, int $length = 160, string $appendix = '...'): st
     $str = trim(preg_replace('/\s+/', ' ', $str));
     return $str . $appendix;
 }
-function remove_wordpress_blocks(string $content): string{
+function b1_mf__remove_wordpress_blocks(string $content): string{
     $str = preg_replace('/<!--.*-->/', '', $content); // Removing Comments (AKA Blocks)
     $str = preg_replace('/\[.*]/', '', $str); // Removing Shortcodes
     $str = strip_tags($str); // Removing HTML
     return trim($str);
+}
+function b1_mf__get_meta_description(WP_Post $post, string $site_name, bool $singular = false, bool $acf_is_active = false): string {
+    $acf_field_used = false;
+    $desc = '';
+    if($singular){
+        $desc = b1_mf__remove_wordpress_blocks($post->post_content); // Try and use post content
+    }
+    if(!$desc && $acf_is_active) {
+        $desc = get_field(B1_MF_ACF_DESC, 'option'); // see if we have ACF and try and use the fallback
+        if($desc) $acf_field_used = true;
+    }
+    if($singular && !$desc) {
+       $desc = trim($post->post_excerpt); // if not use the excerpt
+    }
+    if(!$desc) $desc = get_option('blogdescription'); // if no excerpt use site tagline
+    if(!$desc) $desc = $site_name; // if no tagline use the blog name again.
+    return esc_attr(b1_mf__truncate($desc, 160, $acf_field_used ? '' : '...'));
+}
+function b1_mf__get_meta_image(WP_Post $post, bool $singular = false, bool $acf_is_active = false) : string {
+    $image = '';
+
+    if($singular){
+        $image = esc_attr(get_the_post_thumbnail_url($post->ID, 'full'));
+    }
+    // If no post thumbnail and ACF is active, try to use the fallback
+    if(!$image && $acf_is_active){
+        $image = get_field(B1_MF_ACF_IMAGE, 'option');
+    }
+    // If no post thumbnail still, use the site logo as a backup
+    if(!$image && has_custom_logo()){
+        $logo = get_theme_mod( 'custom_logo' );
+        $attachment = wp_get_attachment_image_src( $logo , 'full' );
+        $image = esc_attr($attachment[0]);
+    }
+
+    return $image;
 }
 
 // Check if Yoast SEO is active
@@ -119,51 +155,46 @@ if( ! in_array( $yoast_plugin_key, $plugin_keys ) ) {
 function meta_fallback(bool $acf_is_active = false) : void
 {
     global $post;
+    global $wp;
+    
+    // Constants
+    $site_name = get_bloginfo('name');
+    $singular = is_singular();
+    $domain = get_bloginfo('url');
 
-    $blog_name = get_bloginfo('name');
-    $post_title = esc_attr($post->post_title . ' | '.$blog_name);
-    $page_url = esc_attr(get_permalink($post->ID));
-    $domain = esc_attr(get_bloginfo('url'));
-    $type = esc_attr((is_single() && $post->post_type === 'post') ? 'article' : 'website');
+    // Vars depending on singularity
+    $url = $singular ? get_permalink($post->ID) : home_url($wp->request);
+    $title = $singular ? $post->post_title . ' | '.$site_name : $site_name;
+    $type = $singular && $post->post_type == 'post' ? 'article' : 'website';
 
-    $acf_field_used = false;
-    $desc = remove_wordpress_blocks($post->post_content); // Try and use post content
-    if(!$desc && $acf_is_active) {
-        $desc = get_field(B1_MF_ACF_DESC, 'option'); // see if we have ACF and try and use the fallback
-        if($desc) $acf_field_used = true;
-    }
-    if(!$desc) $desc = trim($post->post_excerpt); // if not use the excerpt
-    if(!$desc) $desc = get_option('blogdescription'); // if no excerpt use site tagline
-    if(!$desc) $desc = $blog_name; // if no tagline use the blog name again.
-    $description = esc_attr(truncate($desc, 160, $acf_field_used ? '' : '...'));
 
-    $post_thumbnail = esc_attr(get_the_post_thumbnail_url($post->ID, 'full'));
-    // If no post thumbnail and ACF is active, try to use the fallback
-    if(!$post_thumbnail && $acf_is_active){
-        $post_thumbnail = get_field(B1_MF_ACF_IMAGE, 'option');
-    }
-    // If no post thumbnail still, use the site logo as a backup
-    if(!$post_thumbnail && has_custom_logo()){
-        $logo = get_theme_mod( 'custom_logo' );
-        $image = wp_get_attachment_image_src( $logo , 'full' );
-        $post_thumbnail = esc_attr($image[0]);
-    }
+    // Formatting    
+    $title = esc_attr($title);
+    $url = esc_url($url);
+    $domain = esc_url($domain);
+    $type = esc_attr($type);
+
+    // Get More Complex Data
+    $description = b1_mf__get_meta_description($post, $site_name, $singular, $acf_is_active);
+    $image = b1_mf__get_meta_image($post, $singular, $acf_is_active);
+
+    // ==== OUTPUT META TAGS ====
 
     // Basic Meta Description
     echo "<meta name='description' content='{$description}'>";
 
     // Open Graph Meta
-    echo "<meta property='og:url' content='{$page_url}'>";
+    echo "<meta property='og:url' content='{$url}'>";
     echo "<meta property='og:type' content='{$type}'>";
-    echo "<meta property='og:title' content='{$post_title}'>";
+    echo "<meta property='og:title' content='{$title}'>";
     echo "<meta property='og:description' content='{$description}'>";
-    echo "<meta property='og:image' content='{$post_thumbnail}'>";
+    echo "<meta property='og:image' content='{$image}'>";
 
     // Twitter Card Meta
     echo "<meta name='twitter:card' content='summary_large_image'>";
     echo "<meta property='twitter:domain' content='{$domain}'>";
-    echo "<meta property='twitter:url' content='{$page_url}'>";
-    echo "<meta name='twitter:title' content='{$post_title}'>";
+    echo "<meta property='twitter:url' content='{$url}'>";
+    echo "<meta name='twitter:title' content='{$title}'>";
     echo "<meta name='twitter:description' content='{$description}'>";
-    echo "<meta name='twitter:image' content='{$post_thumbnail}'>";
+    echo "<meta name='twitter:image' content='{$image}'>";
 }
